9a0cb08438941410fd0a785776b28dbc1ed99550
Hifni <k8ten.effo@gmail.com>
2025-08-28 21:20:45 +0700

    Initial commit of Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>UPTD Geolab Portofolio</title>
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1 class="text-primary">Selamat Datang di UPTD Geolab</h1>
+                    <p class="lead">Website portofolio ini sedang dalam tahap pengembangan.</p>
+                    <button class="btn btn-primary btn-lg">Mulai Jelajahi</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
